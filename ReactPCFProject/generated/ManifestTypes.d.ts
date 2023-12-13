506c9689d4b16c13f6591f3ae4d8b811638897f9
Joel Alves Pereira <atejap05@yahoo.com.br>
2023-12-13 09:55:56 -0300

    matifestTypes

---
+++ ReactPCFProject/generated/ManifestTypes.d.ts
@@ -0,0 +1,13 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    Título: ComponentFramework.PropertyTypes.StringProperty;
+    Mensagem: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    Título?: string;
+    Mensagem?: string;
+}
